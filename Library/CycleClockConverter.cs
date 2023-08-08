8b03deff0c703b92ff1f72b6f20976848f6c94a8
fchb1239 <fchb1239.dk@gmail.com>
2023-08-08 15:21:14 +0200

    uploaded cycle clock converter for developers

---
+++ Library/CycleClockConverter.cs
@@ -0,0 +1,34 @@
+using System;
+
+public static class CycleClockConverter
+{
+    public static double Percentage(this DateTime value)
+    {
+        double percentage = (DateTime.Now.TimeOfDay.Ticks / 864000000000) * 100;
+        return percentage;
+    }
+
+    public static double Cycle(this DateTime value)
+    {
+        var percentage = Percentage(value);
+        return Math.Truncate(percentage);
+    }
+
+    public static double Moment(this DateTime value)
+    {
+        var percentage = Percentage(value);
+        return Math.Truncate((percentage % 1) * 100);
+    }
+
+    public static double Beat(this DateTime value)
+    {
+        var percentage = Percentage(value);
+        return Math.Truncate((((percentage % 1) * 100) % 1) * 100);
+    }
+
+    public static DateTime CycleToDateTime(double value)
+    {
+        long ticks = (long)(value * 100) / 864000000000;
+        return new DateTime(ticks);
+    }
+}
